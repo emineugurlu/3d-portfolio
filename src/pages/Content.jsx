1f34fd6490668dce1913e586824aed4e8c74eb9c
Emine Uğurlu <emineugurlu957@gmail.com>
2025-04-30 20:10:28 +0300

    Güncelleme:

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 const Content = () => {
   const [form, setForm] = useState({name: '', email: '', message: ''})
   const handleChange = () => {};
+  const hadleFocus = () => {};
+  const handleBlur = () => {};
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
      <div className="flex-1 min-w-[50%] flex flex-col">
@@ -19,7 +21,40 @@ const Content = () => {
          placeholder='Emine'
          required
          value={form.name}
+         onChange={handleChange}
+         onFocus={hadleFocus}
+         onBlur={handleBlur}
         ></input>
+      </label>
+
+      <label className='text-black-500 font-semibold'>
+        Email
+        <input
+         type="email"
+         name="email"
+         className='input'
+         placeholder='ugurlu34emine@gmail.com'
+         required
+         value={form.email}
+         onChange={handleChange}
+         onFocus={hadleFocus}
+         onBlur={handleBlur}
+        ></input>
+      </label>
+
+      <label className='text-black-500 font-semibold'>
+        Your Message
+        <textarea
+         name="message"
+         rows={4}
+         className='input'
+         placeholder='Let me kanow how I can help you!'
+         required
+         value={form.message}
+         onChange={handleChange}
+         onFocus={hadleFocus}
+         onBlur={handleBlur}
+        ></textarea>
       </label>
       </form>
      </div>
