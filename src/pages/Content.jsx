a333d98da437564cfcbb310dabdb83af2f2ccc91
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-02 14:28:56 +0300

    Update

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -13,7 +13,7 @@ const Content = () => {
     e.preventDefault();
     setisLoading(true);
 
-    emailjs.form(
+    emailjs.send(
      import.meta.env.VITE_APP_EMAILJS_ID,
      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
      {
@@ -27,6 +27,7 @@ const Content = () => {
     ).then(() =>{
         setisLoading(false);
 
+      setForm({name: '', email: '', message: ''})  
     }).catch((error) =>{
        setisLoading(false);
        console.log(error);
