f7d1826937dcb548a0e7865cc0fe59f3c8280463
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-03 16:59:11 +0300

    Update

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,24 +1,29 @@
 import React, { useState, useRef, Suspense } from "react";
 import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
-import Fox from "../models/Fox";
+
 import Loader from "../components/Loader";
+import Fox from "../models/Fox";
+import Alert from "../components/Alert";
+import useAlert from "../hooks/useAlert";
 
 const Content = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
-  const [currentAnimation, setCurrentAnimation] = useState("idle"); // 🟢 doğru isim
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-    setCurrentAnimation("walk"); // 🟢 yazarken
+    setCurrentAnimation("walk");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    setCurrentAnimation("hit"); // 🟢 gönderince
+    setCurrentAnimation("hit");
 
     emailjs
       .send(
@@ -34,23 +39,28 @@ const Content = () => {
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(() => {
+        showAlert({ text: "Message sent successfully!", type: "success" });
         setIsLoading(false);
         setTimeout(() => {
-          setCurrentAnimation("idle"); // 🟢 geri dön
+          hideAlert();
+          setCurrentAnimation("idle");
           setForm({ name: "", email: "", message: "" });
         }, 3000);
       })
       .catch((error) => {
-        setIsLoading(false);
-        setCurrentAnimation("walk.left"); // 🟢 hata animasyonu
         console.error("EmailJS Error:", error);
+        setIsLoading(false);
+        setCurrentAnimation("walk.left");
+        showAlert({ text: "I didn't receive your message", type: "danger" });
       });
   };
 
   const handleBlur = () => setCurrentAnimation("idle");
 
   return (
-    <section className="relative flex lg:flex-row flex-col max-container">
+    <section className="relative flex lg:flex-row flex-col max-container min-h-screen">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -107,7 +117,7 @@ const Content = () => {
         </form>
       </div>
 
-      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+      <div className="lg:w-1/2 w-full h-[300px] md:h-[400px]">
         <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
           <directionalLight intensity={2.5} position={[0, 0, 1]} />
           <ambientLight intensity={0.5} />
