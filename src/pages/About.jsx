30a5d97c693a96183c3efb40b6382e63c2d35e69
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-09 17:48:10 +0300

    Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Emine
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Hi, I'm Emine Uğurlu — a passionate frontend developer currently
+          studying Computer Engineering at Isparta University of Applied
+          Sciences. I love creating interactive and user-friendly web interfaces
+          that blend design with functionality. I'm constantly learning new
+          technologies like React.js, and I enjoy turning ideas into clean,
+          responsive websites. I'm excited to keep growing, building, and
+          collaborating on meaningful digital experiences.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
