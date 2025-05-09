d6c0a60b906ac2b79976a13839659a7e8954e980
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-09 18:08:51 +0300

    Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       </h1>
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
-          Hi, I'm Emine Uğurlu — a passionate frontend developer currently
+          Hi, I'm Emine Uğurlu a passionate frontend developer currently
           studying Computer Engineering at Isparta University of Applied
           Sciences. I love creating interactive and user-friendly web interfaces
           that blend design with functionality. I'm constantly learning new
@@ -27,18 +27,27 @@ const About = () => {
           {skills.map((skill) => (
             <div className="block-container w-20 h-20 ">
               <div className="btn-back rounded-xl">
-              <div className="btn-front rounded-xl flex justify-center items-center">
-                <img 
-                src={skill.imageUrl}
-                alt={skill.name}
-                className="w-1/2 h-1/2 object-contain"
-                >
-                
-                </img>
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <img
+                    src={skill.imageUrl}
+                    alt={skill.name}
+                    className="w-1/2 h-1/2 object-contain"
+                  ></img>
                 </div>
-                </div>
+              </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            I'm constantly learning new technologies like React.js, and I enjoy
+            turning ideas into clean, responsive websites. I'm excited to keep
+            growing, building, and collaborating on meaningful digital
+            experiences.
+          </p>
         </div>
       </div>
     </section>
