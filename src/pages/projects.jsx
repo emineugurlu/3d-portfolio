26ce3eedca80f1bbd33037a7e16c0f2dc3f86fc7
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-10 16:34:53 +0300

    Update

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -25,15 +25,33 @@ const Projects = () => {
 
       <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project, index) => (
-          <div className="lg:w-[400px] w-full">
-            <div className={`btn-back rounded-xl ${project.theme}`}>
-              <div>
-                <img
-                  src={project.iconUrl}
-                  alt={project.name || "Project Icon"}
-                  className="w-1/2 h-1/2 object-contain"
-                />
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`}>
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <img
+                    src={project.iconUrl}
+                    alt={project.name || "Project Icon"}
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
               </div>
+            </div>
+            <div className="mt-5 flex flex-col">
+            <h4>
+              {project.name}
+            </h4>
+            <p>
+              {project.description}
+            </p>
+            <div>
+              <Link 
+              to={project.link}
+              target="_blank"
+              >
+                
+              </Link>
+            </div>
             </div>
           </div>
         ))}
