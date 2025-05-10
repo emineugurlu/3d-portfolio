03a33f722959b03aaf801ff0d2f47aa5dbc275ee
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-10 16:15:26 +0300

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import About from "./About";
-import Projects from "./projects";
+import Projects from "./Projects";
 import Content from "./Content";
 
 export { Home, About, Projects, Content };
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const projects = () => {
-  return <div>projects</div>;
+const Projects = () => {
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        My{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Hi, I'm Emine Uğurlu — a frontend developer passionate about crafting
+          intuitive and visually engaging user interfaces. On this page, you'll
+          find a collection of projects that reflect my journey of continuous
+          learning and creativity. I love building responsive, functional, and
+          clean web experiences using modern tools like React.js. Each project
+          represents a step forward in my growth and dedication to frontend
+          development. Let's explore what I've been working on!
+        </p>
+      </div>
+    </section>
+  );
 };
 
-export default projects;
+export default Projects;
