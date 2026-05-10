84f9450dc78f86b78f9d3cc4cdbdbe3185d0bb82
Emine UĞURLU <emineugurlu957@gmail.com>
2026-05-10 19:49:42 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import About from "./About";
-import Projects from "./Projects";
+import Projects from "./projects";
 import Content from "./Content";
 
 export { Home, About, Projects, Content };
